4a5c9d417663123ac7ffc48c2936306430daacb8
Simo Hunter <kingstonegame@gmail.com>
2024-01-25 16:39:25 +0100

    Creation of Main Menu

---
+++ SAS-project.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int choice;
+	
+	printf("welcome to the task management software \n");
+	printf("type a number according to your choice: \n \n");
+	
+	printf("1. to add a task. \n");
+	printf("2. to edit a task. \n");
+	printf("3. to delete a task. \n");
+	printf("2. to filter the tasks. \n");
+	printf("4. to list all the tasks. \n");
+	printf("5. to sort the tasks by date. \n");
+	printf("0. to exit and close the software. \n");
+	
+	return 0;
+}
